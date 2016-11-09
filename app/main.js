8c76be0359b637e36c920217b5e554f8dd5d18eb
Suui <suui.gd@gmail.com>
2016-11-09 20:22:17 +0100

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,12 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+app.on('ready', main);
+
+function main() {
+    const window = new BrowserWindow({ width: 1240, height: 720 });
+    const displayPath = path.join(__dirname, '..');
+    window.loadURL(path.join(displayPath, 'dashboard.html'));
+    window.webContents.openDevTools();
+}
