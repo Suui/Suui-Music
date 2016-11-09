df636da6276f63452b3127aba64274cbc502d882
Suui <suui.gd@gmail.com>
2016-11-09 21:30:32 +0100

    research

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ app.on('ready', main);
 
 function main() {
     const window = new BrowserWindow({ width: 1240, height: 720 });
-    const displayPath = path.join(__dirname, '..');
+    const displayPath = path.join(__dirname, '../display');
     window.loadURL(path.join(displayPath, 'dashboard.html'));
     window.webContents.openDevTools();
 }
